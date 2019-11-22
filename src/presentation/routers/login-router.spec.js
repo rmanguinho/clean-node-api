7240180d020cb38fcaeb86e70309165a310857a7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2019-11-22 18:44:08 -0300

    ensure LoginRouter returns 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,22 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
