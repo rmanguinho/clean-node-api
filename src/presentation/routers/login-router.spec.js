0bb80cdc54101414c6066857fa30ebc2fb9df49b
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2019-11-22 18:49:29 -0300

    remove duplicated code

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,7 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
