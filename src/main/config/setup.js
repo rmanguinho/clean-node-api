45cd2b7593f8f1cd5c40985d02967608faf95b21
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2019-12-09 20:58:16 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,4 +10,15 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
