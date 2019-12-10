b4906e45720188178f933aaebce6b78c700cdc89
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2019-12-09 21:18:04 -0300

    ensure requests return given content-type when forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,15 @@ describe('Content-Type Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
