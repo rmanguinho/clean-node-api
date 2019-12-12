ec5f33d122fb42156563512c7b1f6443abae2832
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2019-12-12 19:16:46 -0300

    add express router adapter

---
+++ src/main/adapters/express-router-adapter.js
@@ -0,0 +1,11 @@
+module.exports = class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,5 +1,6 @@
 const loginRouter = require('../composers/login-router-composer')
+const ExpressRouterAdapter = require('../adapters/express-router-adapter')
 
 module.exports = router => {
-  router.post('/login', loginRouter)
+  router.post('/login', ExpressRouterAdapter.adapt(loginRouter))
 }
